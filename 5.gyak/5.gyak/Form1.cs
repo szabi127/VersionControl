01e9438ca459a12f7d68c7c736273b202afdc64a
szabi127 <szabolcs.gehl@stud.uni-corvinus.hu>
2022-10-21 11:37:18 +0200

    elsolépés

--- 5.gyak/5.gyak/Form1.cs
+++ 5.gyak/5.gyak/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _5.gyak.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,23 @@ namespace _5.gyak
         public Form1()
         {
             InitializeComponent();
+            arfolyam();
+        }
+
+        private void arfolyam()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
