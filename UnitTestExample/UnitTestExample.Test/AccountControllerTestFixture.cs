401110b2db309e68dbd6119f928ff781af2f21a9
szabi127 <szabolcs.gehl@stud.uni-corvinus.hu>
2022-12-09 14:01:52 +0100

    start

--- UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -10,12 +10,14 @@ namespace week8
 {
     public class AccountControllerTestFixture
     {
-        [Test,
-         TestCase("abcd1234", false),
-         TestCase("irf@uni-corvinus", false),
-         TestCase("irf.uni-corvinus.hu", false),
-         TestCase("irf@uni-corvinus.hu", true)
-        ]
+        [
+             Test,
+             TestCase("abcd1234", false),
+             TestCase("irf@uni-corvinus", false),
+             TestCase("irf.uni-corvinus.hu", false),
+             TestCase("irf@uni-corvinus.hu", true)
+         ]
+
         public void TestValidateEmail(string email, bool expectedResult)
         {
             // Arrange
@@ -28,11 +30,35 @@ namespace week8
             Assert.AreEqual(expectedResult, actualResult);
         }
 
+
+        [
+            Test,
+            TestCase("aaaaAA_*", false),    //nincs szám
+            TestCase("AAAA12_*", false),    //nincs kisbetű
+            TestCase("aaa123*_", false),    //nincs nagybetű
+            TestCase("acb1*", false),       //rövid jelszó
+            TestCase("Abc123*_", true),     //megfelelő jelszó
+        ]
+
+        public void TestValidatePassword(string password, bool expectedResult)
+        {
+            // Arrange
+            var accountController = new AccountController();
+
+            // Act
+            var actualResult = accountController.ValidatePassword(password);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+
         [
             Test,
             TestCase("irf@uni-corvinus.hu", "Abcd1234"),
-            TestCase("irf@uni-corvinus.hu", "Abcd1234567"),
+            TestCase("irf@uni-corvinus.hu", "Abcd1234567")
         ]
+
         public void TestRegisterHappyPath(string email, string password)
         {
             // Arrange
@@ -45,6 +71,36 @@ namespace week8
             Assert.AreEqual(email, actualResult.Email);
             Assert.AreEqual(password, actualResult.Password);
             Assert.AreNotEqual(Guid.Empty, actualResult.ID);
+        }
+
+
+        [
+            Test,
+            TestCase("irf@uni-corvinus", "Abcd1234"),
+            TestCase("irf.uni-corvinus.hu", "Abcd1234"),
+            TestCase("irf@uni-corvinus.hu", "abcd1234"),
+            TestCase("irf@uni-corvinus.hu", "ABCD1234"),
+            TestCase("irf@uni-corvinus.hu", "abcdABCD"),
+            TestCase("irf@uni-corvinus.hu", "Ab1234"),
+        ]
+
+        public void TestRegisterValidateException(string email, string password)
+        {
+            // Arrange
+            var accountController = new AccountController();
+
+            // Act
+            try
+            {
+                var actualResult = accountController.Register(email, password);
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                Assert.IsInstanceOf<ValidationException>(ex);
+            }
+
+            // Assert
         }
     }
 }
